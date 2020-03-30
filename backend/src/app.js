ddda647b98ab16f613f9c7fc0378736fda99c55e
Tanguy Roberjot <tanguy.roberjot@eticasolucoes.com.br>
2020-03-30 08:51:59 -0300

    set env vars

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,16 @@ const cors = require('cors');
 const routes = require('./routes');
 const { errors } = require('celebrate');
 const { setupWebSocket } = require('./websocket');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const server = http.Server(app);
 
 setupWebSocket(server);
 
-mongoose.connect('mongodb+srv://devmap:rocketseatdevmap@metrix-hkfkc.mongodb.net/devmap?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@metrix-hkfkc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
